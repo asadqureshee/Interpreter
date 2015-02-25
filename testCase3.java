d44a578b3a4b0713d610b67d4483141984a95800
Asad Qureshi <asad_qureshee@hotmail.com>
2015-02-25 23:40:49 +0500

    Create testCase3.java

---
+++ testCase3.java
@@ -0,0 +1,12 @@
+package lab2;
+
+public class testCase3 {
+//initializing test case
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		interpreter jj = new interpreter();
+		String path = "/Users/asadqureshi/Downloads/sample-3.txt";
+		jj.readFile(path);
+	}
+
+}
