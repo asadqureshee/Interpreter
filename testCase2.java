9ddb3d3466039005283c2d52992aa21591af2a78
Asad Qureshi <asad_qureshee@hotmail.com>
2015-02-25 23:40:29 +0500

    Create testCase2.java

---
+++ testCase2.java
@@ -0,0 +1,12 @@
+package lab2;
+//Multiplication test casse
+public class testCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		interpreter ii = new interpreter();
+		String path = "/Users/asadqureshi/Downloads/sample-2.txt";
+		ii.readFile(path);
+	}
+
+}
